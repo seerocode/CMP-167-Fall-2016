fab7ecd82895f7da1fc062fab36d04d06059ee64
seerocode <rosemarie.liriano@gmail.com>
2016-09-17 00:34:57 -0400

    Changed order of lines and added comments

--- Project-1/src/Secret.java
+++ Project-1/src/Secret.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Secret {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Creates a new scanner
 		
 		System.out.print("Type in your secret message: ");
-		String message = sc.nextLine();
+		String message = sc.nextLine(); //Characters entered are stored in the message variable and goes to the next line
 		
-		int secretNumber = (message.length() * 2) + 7;
+		System.out.printf("The secret message has %d characters.\n", message.length()); //Prints the amount of characters in the user's input message
 		
-		System.out.printf("The secret message has %d characters.\n", message.length());
-		System.out.printf("Your secret number is %d.", secretNumber);
+		int secretNumber = (message.length() * 2) + 7; //Computes secret number 
+		
+		System.out.printf("Your secret number is %d.", secretNumber); 
 
 	}
 
