4ba029aa2127254ad29f35e2b7b4535157375222
seerocode <rosemarie.liriano@gmail.com>
2016-09-18 08:22:10 -0400

    Updated format specifier in statements to include comma

--- Time/src/Time.java
+++ Time/src/Time.java
@@ -13,8 +13,8 @@ public class Time{
 		final int SEC_SINCE_MN = (SEC_IN_HOUR * hour) + (SEC_IN_MIN * minute) + second; //Calculates the seconds since midnight, or 00:00
 		final int SEC_IN_DAY = 24 * SEC_IN_HOUR; //Calculates the amount of seconds in a day; 24 stands for the hours in a day
 		
-		System.out.printf("The number of seconds since midnight is: %d\n", SEC_SINCE_MN);
-		System.out.printf("The number of seconds remaining in the day is: %d\n", SEC_IN_DAY - SEC_SINCE_MN);
+		System.out.printf("The number of seconds since midnight is: %,d\n", SEC_SINCE_MN);
+		System.out.printf("The number of seconds remaining in the day is: %,d\n", SEC_IN_DAY - SEC_SINCE_MN);
 		// Escape percent sign is %%. Added format specifier %.0f since 100.0 is now read as a double number and not an integer as well as to round to the nearest whole number
 		System.out.printf("The percentage of the day that has passed is: %.0f%%", (SEC_SINCE_MN * 100.0) / SEC_IN_DAY); 
 
