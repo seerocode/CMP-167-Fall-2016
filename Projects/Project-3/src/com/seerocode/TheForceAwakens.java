f155b604acb2c91de4d22664fe9b17caf5fff81e
seerocode <rosemarie.liriano@gmail.com>
2016-11-16 17:57:27 -0500

    Deleted comments and edited line spacing

--- Projects/Project-3/src/com/seerocode/TheForceAwakens.java
+++ Projects/Project-3/src/com/seerocode/TheForceAwakens.java
@@ -3,10 +3,11 @@ package com.seerocode;
 public class TheForceAwakens {
 	
 	public static void main(String[] args) {
-		//Harrison Ford portrays Han Solo
-		HanSolo hs = new HanSolo("Harrison Ford"); //resistance
 		
-		hs.setSpouse("Leia Organa"); //resistance
+		HanSolo hs = new HanSolo("Harrison Ford"); 
+		
+		hs.setSpouse("Leia Organa"); 
+		
 		String[] children= {"Ben", "Jaina Solo", "Jacen Solo", "Anakin Solo"};
 		hs.setChildren(children);
 		
