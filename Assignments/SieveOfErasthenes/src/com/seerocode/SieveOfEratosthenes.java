6040656d03f024d452b82b3b746a2fe20b2a1718
seerocode <rosemarie.liriano@gmail.com>
2016-11-30 17:22:43 -0500

    Edited comment

--- Assignments/SieveOfErasthenes/src/com/seerocode/SieveOfEratosthenes.java
+++ Assignments/SieveOfErasthenes/src/com/seerocode/SieveOfEratosthenes.java
@@ -10,11 +10,11 @@ public class SieveOfEratosthenes {
 	    isPrimeNum[0] = false;
 	    isPrimeNum[1] = false;
 	    for (int i = 2; i < n; i++) { //loops through boolean array starting at two because 0 and 1 are not prime
-	        isPrimeNum[i] = true; // the number at index i will be true if its prime
+	        isPrimeNum[i] = true; // the number at index i set to true
 	    }
 
 	    //loops through array and checks if the counter
-	    // is less than the square root of n (like the sieve)
+	    // is less than the square root of n (the sieve)
 	    for (int num = 2; num < Math.sqrt(n); num++) { 
 	        if (isPrimeNum[num]) { //when num is in the isPrimeNum boolean array
 	        	//sets a multiple of num by multiplying by 2, if the multiple is less than n, sets the number to false
